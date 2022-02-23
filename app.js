9c5805d60783c1f3e5282e5467746cb1d80c06e6
adas <ajanta744@gmail.com>
2022-02-23 11:31:49 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express= require('express');
+
+const app = express()
+
+const port =  7000
+
+//all static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+//template engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+//rputes for app
+app.get('/', (req,res) => {  
+    res.render('index');
+});
+app.get('/about', (req,res) => {
+    res.render('about.ejs');
+
+});
+app.get('/service', (req,res) => {
+    res.render('service.ejs');
+
+});
+app.get('/blog', (req,res) => {
+    res.render('blog');
+
+});
+app.get('/*', (req,res) => {
+    res.render('index');
+
+})
+//listen to the port
+app.listen(port, ()=>{
+    console.log('listening to the port')
+})
